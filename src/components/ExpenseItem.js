a862a46d133b4da02fc41ed330935085ca022cbc
misho <mshaklev@gmail.com>
2024-02-23 08:20:31 -0500

    third

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { TiDelete } from 'react-icons/ti';
+import { FaTimesCircle } from 'react-icons/fa';
 import { AppContext } from '../context/AppContext';
 const ExpenseItem = (props) => {
     const { dispatch } = useContext(AppContext);
@@ -19,12 +19,23 @@ const ExpenseItem = (props) => {
             payload: expense
         });
     }
+    const decreaseAllocation = (name) => {
+        const expense = {
+            name: name,
+            cost: 10,
+        };
+        dispatch({
+            type: 'RED_EXPENSE',
+            payload: expense,
+        });
+    }
     return (
         <tr>
         <td>{props.name}</td>
         <td>£{props.cost}</td>
         <td><button onClick={event=> increaseAllocation(props.name)}>+</button></td>
-        <td><TiDelete size='1.5em' onClick={handleDeleteExpense}></TiDelete></td>
+        <td><button onClick={event=> decreaseAllocation(props.name)}>-</button></td>
+        <td><FaTimesCircle size='2.2em' color="red" onClick={handleDeleteExpense}></FaTimesCircle></td>
         </tr>
     );
 };
