0dd19d3a170bfbabad507de65137bd41697be00a
Mihail Shaklev <60180238+mihailShaklev@users.noreply.github.com>
2024-02-29 13:58:59 +0200

    Update Location.js

--- src/components/Location.js
+++ src/components/Location.js
@@ -1,56 +1,25 @@
 import React, { useContext } from 'react';
 import { AppContext } from '../context/AppContext';
-//import Form from 'react-bootstrap/Form';
-import Select, {components} from 'react-select';
+import Form from 'react-bootstrap/Form';
 const Location = () => {
-  const {dispatch} = useContext(AppContext);
+  const {dispatch, currencyName} = useContext(AppContext);
     const changeLocation = (val, currencyName)=>{
             dispatch({
                 type: 'CHG_CURRENCY',
                 payload: {'val':val, 'currencyName':currencyName},
             })
-    }
-    const options=[
-        {value:"$", label:"$ Dollar"},
-        {value:"£", label:"£ Pound"},
-        {value:"€", label:"€ Euro"},
-        {value:"₹", label:"₹ Rupee"},
-    ];
 
-    const VALUE_PREFIX = "Currency(";
-    const VALUE_SUFFIX = ")";    
+    }
 
   return (
         <div className='alert alert-secondary'> Location { 
-        <Select
-            options={options}
-            placeholder='All'
-            isClearable
-            components={{
-                SingleValue:({children, ...props}) =>{
-                    return(
-                        <components.SingleValue{...props}>
-                        {VALUE_PREFIX + children + VALUE_SUFFIX}
-                        </components.SingleValue>
-                    );
-                },
-                Placeholder:({children,...props}) => {
-                    return(
-                        <components.Placeholder{...props}>
-                        {VALUE_PREFIX + children + VALUE_SUFFIX}
-                        </components.Placeholder>
-                    );
-                },
-                IndicatorSeparator: () => null
-            }}
-        />
-      //<Form.Select name="Location" id="Location" style={{backgroundColor: "#ccffeb"}} onChange={event=>changeLocation(event.target.value, event.target.innerHTML)}>
-        //<option value="" disabled selected hidden>{currencyName}</option>
-        //<option value="$">$ Dollar</option>
-        //<option value="£">£ Pound</option>
-        //<option value="₹">₹ Rupee</option>
-        //<option value="€">€ Euro</option>
-      //</Form.Select>	
+        
+      <Form.Select defaultValue={currencyName} name="Location" id="Location" style={{backgroundColor: "#ccffeb"}} onChange={event=>changeLocation(event.target.value, event.target.title)}>
+        <option value="$" title="$ Dollar">$ Dollar</option>
+        <option value="£" title="£ Pound">£ Pound</option>
+        <option value="₹" title="₹ Rupee">₹ Rupee</option>
+        <option value="€" title="€ Euro">€ Euro</option>
+      </Form.Select>	
       }	
     </div>
     );
