5056ca56d95a2b01d84b6392e3551d82e5361b8d
mish <mshaklev@gmail.com>
2024-02-29 08:13:27 -0500

    last

--- src/components/Location.js
+++ src/components/Location.js
@@ -3,10 +3,10 @@ import { AppContext } from '../context/AppContext';
 import Form from 'react-bootstrap/Form';
 const Location = () => {
   const {dispatch, currencyName} = useContext(AppContext);
-    const changeLocation = (val, currencyName)=>{
+    const changeLocation = (val)=>{
             dispatch({
                 type: 'CHG_CURRENCY',
-                payload: {'val':val, 'currencyName':currencyName},
+                payload: val,
             })
 
     }
@@ -14,7 +14,8 @@ const Location = () => {
   return (
         <div className='alert alert-secondary'> Location { 
         
-      <Form.Select defaultValue={currencyName} name="Location" id="Location" style={{backgroundColor: "#ccffeb"}} onChange={event=>changeLocation(event.target.value, event.target.title)}>
+      <Form.Select value={currencyName} name="Location" id="Location" style={{backgroundColor: "#ccffeb"}} onChange={event=>changeLocation(event.target.value)}>
+        <option hidden selected>{currencyName}</option>
         <option value="$" title="$ Dollar">$ Dollar</option>
         <option value="£" title="£ Pound">£ Pound</option>
         <option value="₹" title="₹ Rupee">₹ Rupee</option>
--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -81,9 +81,20 @@ export const AppReducer = (state, action) => {
             };
         case 'CHG_CURRENCY':
             action.type = "DONE";
-            state.currency = action.payload.val;
-            state.currencyName = 'Currency(' + action.payload.currencyName + ')';
-            console.log(state.currencyName);
+            state.currency = action.payload;
+            if(state.currency === '$'){
+                state.currencyName = 'Currency($ Dollar)';
+            }
+            if(state.currency === '₹'){
+                state.currencyName = 'Currency(₹ Rupee)';
+            }
+            if(state.currency === '€'){
+                state.currencyName = 'Currency(€ Euro)';
+            }
+            if(state.currency === '£'){
+                state.currencyName = 'Currency(£ Pound)';
+            }
+            
             return {
                 ...state
             }
